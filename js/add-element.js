76a3c016c3b4cd076eda900b42b5e5d642542e1f
989onan <onanchew@gmail.com>
2021-11-08 13:17:57 -0500

    1 file modified

--- js/add-element.js
+++ js/add-element.js
@@ -15,13 +15,22 @@ $('#one').next().next().text('milk');
 $('#todo').append('<li>candy</li>');
 // add a new element by clicking the plus sign
 
+document.getElementById('add').addEventListener('click', addElement);
 // before and after are for siblings
 // append and prepend are for parent
 
 function addElement() {
   // add a new element
+
   // add a input text box
+  $('#todo').append('<li><input type="text"></li>');
   // whenever the user are done add the element
+  $('input').blur(function () {
+    $(this).parent().addClass('cool');
+    var items = $(this).val();
+    $(this).parent().text(items);
+    $('li').click(changeStyle);
+  });
 }
 
 // bind click with the event handler
@@ -45,4 +54,5 @@ document.getElementById('remove').addEventListener('click', removeElement);
 
 function removeElement() {
   // remove the marked elements  -- element with style complete
+  $('li.complete').remove();
 }
